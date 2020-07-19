93ad76132ce704750c494b6f25ef60f6cc064fd1
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-07-19 06:16:36 +0100

    Initial set up for Vue3 RC

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto mt-10">
+    <HelloWorld msg="Welcome to Your Vue.js App" v-bind:seen="false"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import './assets/main.css'
 
 export default {
   name: 'App',
@@ -15,14 +15,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
