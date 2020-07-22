9792c65d8121404a3deb09fd5f05deb2e85b709d
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-07-22 20:38:13 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: ['./src/**/*.html', './src/**/*.vue', '.src/**/*.js']
+  },
   target: 'relaxed',
   prefix: '',
   important: false,
