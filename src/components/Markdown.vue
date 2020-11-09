76e724c23fc61f37ffcc67a458001cfc8f9d8e32
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-11-09 12:46:36 +0000

    Add markdown vue

---
+++ src/components/Markdown.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class='w-full flex h-screen'>
+      <article class='w-1/2'>
+        <textarea class='w-full h-full' v-model='text'></textarea>
+      </article>
+      <article class='w-1/2 border bg-gray-200' v-html="markedText">        
+      </article>
+  </section>
+</template>
+
+<script>
+import marked from 'marked';
+export default {
+    data() {
+        return {
+            text: '',
+        }
+    },
+    computed: {
+        markedText() {
+            return marked(this.text);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
