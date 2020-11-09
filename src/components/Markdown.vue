112efd6a022f8f48727362f0879bab380d944f40
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-11-09 20:26:45 +0000

    Complete Markdown App Add mixins

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,25 +1,34 @@
 <template>
   <section class='w-full flex h-screen'>
       <article class='w-1/2'>
-        <textarea class='w-full h-full' v-model='text'></textarea>
+        <textarea class='w-full border h-full outline-none p-2' :value='text' @input="update"></textarea>
       </article>
-      <article class='w-1/2 border bg-gray-200' v-html="markedText">        
+      <article class='w-1/2 border bg-gray-200 p-2' v-html="markedText">        
       </article>
   </section>
 </template>
 
 <script>
 import marked from 'marked';
+import debounce from '../utilities/mixins/debouce';
 export default {
+    // mixins will merge the data from the mixin and this component... baller!
+    mixins: [debounce],
     data() {
         return {
-            text: '',
+            text: '',        
         }
     },
     computed: {
         markedText() {
             return marked(this.text);
         }
+    },
+    methods: {
+        update(e) {
+            const task = () => (this.text = e.target.value);
+            this.debounce(task, 500);
+        },
     }
 }
 </script>
